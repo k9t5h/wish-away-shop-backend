34fdd15d4890f537ab2fc1cb53ed04282b9267c8
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 10:40:31 +0200

    fix: fix CartRepository type.

--- cart-service/src/main/java/com/codecool/cartservice/repository/CartRepository.java
+++ cart-service/src/main/java/com/codecool/cartservice/repository/CartRepository.java
@@ -3,5 +3,5 @@ package com.codecool.cartservice.repository;
 import com.codecool.cartservice.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Long, Cart> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
