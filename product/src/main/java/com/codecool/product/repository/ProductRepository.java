749448497799dcf613d7a93a7b4fa9ef8ec9f435
Kriszti Toth <k95toth@gmail.com>
2021-05-18 16:19:04 +0200

    fix: find only unsold products

--- product/src/main/java/com/codecool/product/repository/ProductRepository.java
+++ product/src/main/java/com/codecool/product/repository/ProductRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findAllByCategory(Category category);
+    List<Product> findAllByCategoryAndSoldFalse(Category category);
+    List<Product> findAllBySoldFalse();
 
 }
