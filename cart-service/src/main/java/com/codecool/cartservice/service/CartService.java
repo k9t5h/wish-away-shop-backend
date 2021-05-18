f63240b9c29a517de264af2c1c897dc06c5f7811
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 10:55:21 +0200

    feat: create CartService.

---
+++ cart-service/src/main/java/com/codecool/cartservice/service/CartService.java
@@ -0,0 +1,50 @@
+package com.codecool.cartservice.service;
+
+import com.codecool.cartservice.model.Cart;
+import com.codecool.cartservice.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CartService {
+
+    private CartRepository cartRepository;
+
+    @Autowired
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public Cart getCart() {
+        return cartRepository.findAll().get(0);
+    }
+
+    public Cart addProductToCart(long productId) {
+        Cart cart = getCart();
+        List<Long> productIds = cart.getProductIds();
+        productIds.add(productId);
+        cartRepository.save(cart);
+        return cart;
+    }
+
+    public void removeProductFromCart(long productId) {
+        Cart cart = getCart();
+        List<Long> productIds = cart.getProductIds();
+        productIds.remove(productId);
+        cartRepository.save(cart);
+    }
+
+    public void emptyCart() {
+        Cart cart = getCart();
+        cart.setProductIds(new ArrayList<>());
+    }
+
+    public Cart getCartWithProducts() {
+        Cart cart = getCart();
+        
+        return cart;
+    }
+}
