c30ea9f613c936411c8c4ed9dd3a1e1c1441d228
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 11:35:35 +0200

    feat: create default empty cart in CartServiceApplication.

--- cart-service/src/main/java/com/codecool/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/codecool/cartservice/CartServiceApplication.java
@@ -1,5 +1,9 @@
 package com.codecool.cartservice;
 
+import com.codecool.cartservice.model.Cart;
+import com.codecool.cartservice.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -7,9 +11,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class CartServiceApplication {
+
+	private CartRepository cartRepository;
+
+	@Autowired
+	public CartServiceApplication(CartRepository cartRepository) {
+		this.cartRepository = cartRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
@@ -19,6 +32,15 @@ public class CartServiceApplication {
 	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public CommandLineRunner init() {
+		return args -> {
+			Cart cart = Cart.builder().productIds(new ArrayList<>()).products(new ArrayList<>()).build();
+			cartRepository.save(cart);
+		};
+
 	}
 
 }
