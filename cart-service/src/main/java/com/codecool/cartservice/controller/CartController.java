b558993f622c47ee0f5cc04248efc07880436696
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 10:55:03 +0200

    feat: create CartController, to handle requests.

---
+++ cart-service/src/main/java/com/codecool/cartservice/controller/CartController.java
@@ -0,0 +1,39 @@
+package com.codecool.cartservice.controller;
+
+import com.codecool.cartservice.model.Cart;
+import com.codecool.cartservice.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/cart")
+public class CartController {
+
+    private CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping(path = "/")
+    public Cart getCart() {
+        return cartService.getCartWithProducts();
+    }
+
+    @PutMapping(path = "/add/{id}")
+    public Cart addProductToCart(@PathVariable(name = "id") long productId) {
+        return cartService.addProductToCart(productId);
+    }
+
+    @DeleteMapping(path = "/remove/{id}")
+    public void removeProductFromCart(@PathVariable(name = "id") long productId) {
+        cartService.removeProductFromCart(productId);
+    }
+
+    @PutMapping(path = "/empty")
+    public void emptyCart() {
+        cartService.emptyCart();
+    }
+
+}
