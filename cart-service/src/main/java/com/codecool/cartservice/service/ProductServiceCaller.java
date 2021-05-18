d9f8bee7ea72fa86492474a058f6699c07122f68
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 11:03:25 +0200

    feat: create ProductServiceCaller.

---
+++ cart-service/src/main/java/com/codecool/cartservice/service/ProductServiceCaller.java
@@ -0,0 +1,34 @@
+package com.codecool.cartservice.service;
+
+import com.codecool.cartservice.model.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductServiceCaller {
+
+    private RestTemplate restTemplate;
+
+    @Value("${product-service.url}")
+    private String productUrl;
+
+    @Autowired
+    public ProductServiceCaller(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<ProductDTO> getProducts(List<Long> productIds){
+        List<ProductDTO> products = new ArrayList<>();
+        for(Long productId: productIds){
+            ProductDTO productDTO = restTemplate.getForEntity(productUrl + "/" + productId, ProductDTO.class).getBody();
+            products.add(productDTO);
+        }
+        return products;
+    }
+}
