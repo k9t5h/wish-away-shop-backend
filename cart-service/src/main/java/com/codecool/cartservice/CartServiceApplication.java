0f17697498db7e65b20ddb113b404ae50ad2ac2a
Kriszti Toth <k95toth@gmail.com>
2021-05-20 19:47:07 +0200

    fix: remove load balancer from order and card services

--- cart-service/src/main/java/com/codecool/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/codecool/cartservice/CartServiceApplication.java
@@ -2,7 +2,6 @@ package com.codecool.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +15,6 @@ public class CartServiceApplication {
 	}
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
